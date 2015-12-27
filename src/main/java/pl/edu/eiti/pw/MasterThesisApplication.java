dc69e79c29ddec6eb69efdf1f1bc66dcc0db04d5
Simonowicz <Simonowicz@gmail.com>
2015-12-27 20:06:21 +0100

    Added spring boot structure

---
+++ src/main/java/pl/edu/eiti/pw/MasterThesisApplication.java
@@ -0,0 +1,12 @@
+package pl.edu.eiti.pw;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MasterThesisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MasterThesisApplication.class, args);
+    }
+}
